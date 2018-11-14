39b005ffad22cc0a72463edaf091a4f08221d9ef
mccullen <mccullen_j@lynchburg.edu>
2018-11-14 08:05:57 -0500

    Add frontend mds script template using class survey data.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"#fake-news" 
+Start Server: python -m http.server
+    - localhost:8000
---
+++ fake-news-mds.js
@@ -0,0 +1,93 @@
+d3.csv("survey.csv").then(plot);
+
+function plot(table) {
+    var graph = TableToGraph(table);  // See docs below about graph data structure.
+    console.log(graph);  // See graph contents in browser console.
+
+    // Set up the D3 Force layout.
+    // Simulation will compute 2D locations of vertices as graph.vertices[i].x and .y
+    var forces = d3.forceSimulation(graph.vertices)
+        .force("spring", d3.forceLink(graph.edges).distance(edge => 10*edge.distance)
+            // Set various useful properties of the forceLink here...
+            // Hint: give .distance() an appropriate function.
+
+            )
+        // What other forces would be useful here?
+        .force("repel", d3.forceManyBody())
+        .force("center", d3.forceCenter(400,300))
+        .on("tick", ticked);
+
+    var scale = d3.scaleLinear().domain([250, 16000]).range([5, 20]);
+
+    function ticked() {
+        // Use D3 here to create/modify DOM objects based on the graph data...
+        // Visualize students' Name and Born attributes.
+        // Add some interaction.
+        d3.select("svg").selectAll("line")
+            .data(graph.edges)
+                .attr("x1", link => link.source.x)
+                .attr("y1", link => link.source.y)
+                .attr("x2", link => link.target.x)
+                .attr("y2", link => link.target.y)
+            .enter().append("line");
+
+        d3.select("svg").selectAll("circle")
+            .data(graph.vertices)
+                .attr("cx", node => node.x)
+                .attr("cy", node => node.y)
+            .enter().append("circle")
+                .attr("r", d => scale(d.Born))
+                .on("mouseover", function (d, i) {
+                        d3.select(this).classed("highlight", true);
+                        d3.select("#details").html("Name: " + d.Name + ", Shoe Size: " + d.Shoe);
+                })
+                .on("mouseout", function (d, i) {
+                        d3.select(this).classed("highlight", false);
+                });
+
+        // Make text labels for dots:
+    d3.select("svg").selectAll("text")
+        .data(graph.vertices)
+                    .attr("x", node => node.x)
+            .attr("y", node => node.y)
+            .text(node => node.Name)
+        .enter().append("text");
+    }
+
+
+    ////////////// Completed Code - no need to edit ///////////////
+
+    // Create data structure for a complete (all pairs) weighted graph from a csv table.
+    // table = array of n rows, as read by d3.csvParse().
+    // Returns graph object where:
+    //	graph.vertices = array of n vertices, each vertex is dict of attr:value pairs from data table.
+    //  graph.edges = array of n(n-1)/2 edges, where each edge contains:
+    //		edge.source, edge.target = references to vertex objects in graph.vertices.
+    //		edge.distance = Distance(source, target).
+    //  graph.attrs = array of attribute names in the table, not including "Name".
+    //  graph.norms = dict of zscore normalization factors for each attribute, for Distance().
+    function TableToGraph(table) {
+        var graph = {'vertices':table, 'edges':[], 'norms':{}, 'attrs':Object.keys(table[0])};
+        graph.attrs.splice(graph.attrs.indexOf('Name'), 1);  //remove "Name"
+        // Compute zscore norms for each attribute:
+        for (var attr of graph.attrs)
+            graph.norms[attr] = d3.deviation(table, row => row[attr]);
+        // Compute all-pairs edges as array of {source:ref, target:ref, distance:value}
+        table.forEach( (src, isrc) =>
+            table.forEach( (dst, idst) => {
+                if(isrc < idst)	 // no duplicate edges
+                    graph.edges.push( {'source': src, 'target': dst, 'distance': Distance(src,dst,graph.norms)} );
+        }));
+        return graph;
+    }
+
+    // Distance metric, computes normalized high-dimensional distance between 2 rows in the table.
+    // r1, r2 = references to row (vertex) objects.
+    // norms = the attribute zscore normalization factors.
+    function Distance(r1, r2, norms) {
+        return d3.sum(Object.keys(norms).map(attr =>
+                Math.abs(r1[attr] - r2[attr]) / norms[attr] ));  // L1
+        //return Math.sqrt(d3.sum(Object.keys(norms).map(attr =>
+        //		Math.pow((r1[attr] - r2[attr])/norms[attr], 2)))); // L2
+    }
+}
