6450bfe75c32fb29303d65bc1f8d6871cf2610e1
mccullen <mccullen_j@lynchburg.edu>
2018-11-18 10:48:22 -0500

    Add checkbox for selection attributes

--- fake-news-mds.js
+++ fake-news-mds.js
@@ -53,7 +53,7 @@ APP.plotComplete = async function () {
 
 
         var graph = TableToGraph();  // See docs below about graph data structure.
-        console.log(APP.table);  // See graph contents in browser console.
+        console.log(graph);  // See graph contents in browser console.
 
         // Set up the D3 Force layout.
         // Simulation will compute 2D locations of vertices as graph.vertices[i].x and .y
@@ -113,13 +113,12 @@ APP.plotComplete = async function () {
         //  graph.attrs = array of attribute names in the table, not including "Name".
         //  graph.norms = dict of zscore normalization factors for each attribute, for Distance().
         function TableToGraph() {
-            var graph = {'vertices':APP.table, 'edges':[], 'norms':{}, 'attrs':Object.keys(APP.table[0])};
-            //graph.attrs.splice(graph.attrs.indexOf('Name'), 1);  //remove "Name"
-            graph.attrs.splice(graph.attrs.indexOf('Author'), 1);  //remove "Name"
-            graph.attrs.splice(graph.attrs.indexOf('URL'), 1);  //remove "Name"
-            graph.attrs.splice(graph.attrs.indexOf('Title'), 1);  //remove "Name"
-            graph.attrs.splice(graph.attrs.indexOf('Description'), 1);  //remove "Name"
-            graph.attrs.splice(graph.attrs.indexOf('Time'), 1);  //remove "Name"
+            var selectedAttributes = $(".attribute-selection input:checked")
+                .map(function() {
+                    return this.value;
+                }).get();
+            console.log(selectedAttributes);
+            var graph = {'vertices':APP.table, 'edges':[], 'norms':{}, 'attrs': selectedAttributes};
             // Compute zscore norms for each attribute:
             for (var attr of graph.attrs)
                 graph.norms[attr] = d3.deviation(APP.table, row => row[attr]);
@@ -169,7 +168,12 @@ $(document).ready(function() {
             });
         }
     });
+
+    $(".attribute-selection input").on("click", event => {
+        APP.plotComplete();
+    });
 });
+
 
 APP.addArticle = function (article) {
     APP.table.push(article);
